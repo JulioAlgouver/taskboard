83c0c9329a3bd54c52fbb342c5557a171199fdb9
Julio Algouver <julioalgouver@gmail.com>
2025-09-28 10:11:08 -0300

    v1.0

--- src/app/front/main/main.component.ts
+++ src/app/front/main/main.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CategoriaComponent } from "../../features/categorias/view/categoria/categoria.component";
+import {MatDividerModule} from '@angular/material/divider';
+import { TarefaComponent } from "../../features/tarefas/view/tarefa/tarefa.component";
+
+
+const COMPONENTS = [CategoriaComponent, TarefaComponent];
+
+const MODULES = [MatDividerModule];
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [...COMPONENTS, ...MODULES],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
